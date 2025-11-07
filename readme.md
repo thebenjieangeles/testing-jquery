577ab6031ffeb5ad967700134d3c4e435f4f213b
thebenjieangeles <thebenjangeles@gmail.com>
2025-11-07 14:47:39 +0800

    event listeners

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 ## I'm back and testing JQuery
+
+1. Tried selecting elements with JQuery
+2. Tried manipulating texts with JQuery
+3. Tried Manipulating attributes using JQuery
--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-$("a").attr("href", "https://www.yahoo.com");
+// EVENT LISTENERS
+
+// this changes the colors of the h1 when any of the buttons are clicked
+$(".button-1").click(function () {
+  $("h1").css("color", "purple");
+});
+
+$(".button-2").click(function () {
+  $("h1").css("color", "red");
+});
+
+$(".button-3").click(function () {
+  $("h1").css("color", "green");
+});
+
+$(".button-4").click(function () {
+  $("h1").css("color", "blue");
+});
+
+$(".button-5").click(function () {
+  $("h1").css("color", "black");
+});
+
+$("input").keypress(function (event) {
+  console.log(event.key);
+});
+
+// This prints out the key pressed on the h1 property
+$(document).keypress(function (event) {
+  $("h1").text(event.key);
+});
+
+// this changes the color of the h1 on hover to green
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "green");
+});
+
+// this adds a new html element
+$("h1").before("<button>new</button>");
+$("h1").after("<button>new</button>");
+$("h1").append("<button>new</button>");
+$("h1").prepend("<button>new</button>");
